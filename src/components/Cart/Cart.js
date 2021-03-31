29cf70ba3bf37791be480500f55b603d4112b3f2
Munni26 <tazneen99@gmail.com>
2021-03-31 09:05:19 +0600

    add

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -3,13 +3,13 @@ import React from 'react';
 const Cart = (props) => {
     const cart = props.cart;
     let total = 0;
-    for(let i = 0; i < cart.length; i++){
+    for (let i = 0; i < cart.length; i++) {
         const course = cart[i];
-        total = total + course.course_price;   
+        total = total + course.course_price;
     }
     return (
         <div>
-            <h4>Order Summary</h4>
+            <h4>Order Sumarry</h4>
             <p>Enrolled Course: {cart.length} </p>
             <p>Total: {total}</p>
         </div>
